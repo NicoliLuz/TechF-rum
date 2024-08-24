17d30cb165362dd7a329984661b6518073e5552b
NicoliLuz <luznicoli@gmail.com>
2024-08-24 15:02:07 -0300

    testefinal

--- front/lerMais.js
+++ front/lerMais.js
@@ -1,11 +1,12 @@
 function lerMais () {
    var spanConteudo = document.getElementById("hidden-conteudo");
    
-   if (spanConteudo.style.display === "none") {
-      spanConteudo.style.display = "block";
-      document.getElementById('btnLerMais').innerHTML = "Reduzir";
+   if (spanConteudo.style.display === "none") { /*Torna o elemento oculto*/
+      spanConteudo.style.display = "block"; /*Torna o elemento visível e muda o texto do botão para "Reduzir"*/
+      document.getElementById('btnLerMais').innerHTML = "Reduzir"; /*Atualiza o texto do botão com o ID btnLerMais para "Reduzir"*/
    } else {
       spanConteudo.style.display = "none";
+      /*Se o elemento estiver visível, oculta-o e muda o texto do botão para "Ler mais"*/
       document.getElementById('btnLerMais').innerHTML = "Ler mais";
    }
 }
