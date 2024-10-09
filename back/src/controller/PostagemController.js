b22fbc834697f4e79c49526c0cb08be94f0da3a5
NicoliLuz <luznicoli@gmail.com>
2024-10-08 21:57:23 -0300

    atualizado lista de comentarios na ramificacao

--- back/src/controller/PostagemController.js
+++ back/src/controller/PostagemController.js
@@ -24,6 +24,50 @@ async function addComentarioAutomacao(request, response) {
     await addComentario(request, response, 'AUTOMACAO');
 }
 
+async function listarComentariosRamificacao(request, response) {
+    await listarComentarios(request, response, 'RAMIFICACAO');
+}
+
+// Tabela postagens
+function listarComentarios(request, response, pagina) {
+    try {
+        // Recuperando Formulário
+        const params = Array(
+            pagina
+        );
+
+        /*Confirma se função está sendo chamada*/
+        console.log("dentro da api");
+        console.log("nome: ", request.body.nome);
+        console.log("textoPost: ", request.body.textoPost);
+
+        const query = "SELECT * FROM postagens WHERE pagina = ? ORDER BY datapost DESC LIMIT 50";
+
+        /*Faz uma consulta no mySQL*/
+        connection.query(query, params, (err, results) => {
+            if (results) { /*Se a inserção for bem-sucedida, responde com status 201 */
+                response
+                    .status(200)
+                    .json({
+                        success: true,
+                        message: "Sucesso!",
+                        data: results
+                    })
+            } else { /*Se ocorrer um erro, responde com status 400 e mensagem de erro*/
+                response
+                    .status(400)
+                    .json({
+                        sucess: false,
+                        message: "Ops, deu problema!",
+                        data: err
+                    })
+            }
+        })
+    } catch (error) { /*Se ocorrer um erro na execução da função, exibe no console*/
+        console.log(error);
+    }
+}
+
 // Tabela postagens
 function addComentario(request, response, pagina) {
     try {
@@ -71,5 +115,6 @@ module.exports = {
     addComentarioMedia,
     addComentarioEmpresa,
     addComentarioSkill,
-    addComentarioAutomacao
+    addComentarioAutomacao,
+    listarComentariosRamificacao
 }
--- back/src/routes/dbRouter.js
+++ back/src/routes/dbRouter.js
@@ -10,7 +10,8 @@ const { addComentarioRamificacao } = require('../controller/PostagemController')
 const { addComentarioMedia } = require('../controller/PostagemController');
 const { addComentarioEmpresa } = require('../controller/PostagemController');
 const { addComentarioSkill } = require('../controller/PostagemController');
-const {   addComentarioAutomacao } = require('../controller/PostagemController');
+const { addComentarioAutomacao } = require('../controller/PostagemController');
+const { listarComentariosRamificacao } = require('../controller/PostagemController');
 
 router.post('/usuario/criar', criarUsuario);
 router.post('/usuario/logar', logarUsuario);
@@ -20,6 +21,7 @@ router.post('/post/addComentarioMedia', addComentarioMedia);
 router.post('/post/addComentarioEmpresa', addComentarioEmpresa);
 router.post('/post/addComentarioSkill', addComentarioSkill);
 router.post('/post/addComentarioAutomacao', addComentarioAutomacao);
+router.get('/get/listarComentariosRamificacao', listarComentariosRamificacao);
 
 module.exports = router;
 
